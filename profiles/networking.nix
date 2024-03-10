a24ea21daf561f238974e8fac5f75acce5aa3b36
Matei Dibu <contact@mateidibu.dev>
2024-03-10 19:39:50 +0200

    profiles: networking: disable ipv6

--- profiles/networking.nix
+++ profiles/networking.nix
@@ -4,6 +4,8 @@
     useDHCP = false;
     networkmanager.enable = false;
     nftables.enable = true;
+    # temporarily off while resolving route config
+    enableIPv6 = false;
   };
   systemd.network.enable = true;
 }
