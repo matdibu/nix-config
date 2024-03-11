9cd46b99e17f2ed751f72f6903779f98fc7765e3
Matei Dibu <contact@mateidibu.dev>
2024-03-11 11:30:51 +0200

    profiles: nvidia: cleanup

--- profiles/nvidia.nix
+++ profiles/nvidia.nix
@@ -18,9 +18,10 @@
     ];
   };
 
-  services.xserver.videoDrivers = ["modesetting" "nvidia"];
+  services.xserver.videoDrivers = ["nvidia"];
 
-  boot.initrd.kernelModules = [
+  # boot.initrd.kernelModules = [
+  boot.kernelModules = [
     "nvidia"
     "nvidia_modeset"
     "nvidia_uvm"
@@ -28,13 +29,21 @@
   ];
 
   boot.kernelParams = lib.mkBefore [
-    "nvidia_drm.modeset=1"
     "nvidia_drm.fbdev=1"
   ];
 
   hardware.nvidia = {
+    # Modesetting is required.
     modesetting.enable = true;
+
+    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
+    # Enable this if you have graphical corruption issues or application crashes after waking
+    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
+    # of just the bare essentials.
+    powerManagement.enable = false;
+
     open = true;
-    package = lib.mkDefault config.boot.kernelPackages.nvidiaPackages.beta;
+    nvidiaSettings = false;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
   };
 }
