d534be83daf80e44ccb1b6e8f61d1da050ac9185
Matei Dibu <contact@mateidibu.dev>
2024-03-14 13:15:56 +0200

    hosts: nix-hv: set root disk device

--- hosts/nix-hv/hardware-configuration/disks/default.nix
+++ hosts/nix-hv/hardware-configuration/disks/default.nix
@@ -3,4 +3,6 @@
     inputs.self.nixosModules.profiles-impermanence
     ./impermanence.nix
   ];
+
+  disko.devices.disk."root".device = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_1TB_S4EWNF0M943331J";
 }
