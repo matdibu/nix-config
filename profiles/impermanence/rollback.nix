79d73d1c5726a2e2db7d8b46db172c139c169c7a
Matei Dibu <contact@mateidibu.dev>
2024-03-14 11:41:45 +0200

    profiles: impermanence: fix rollback

--- profiles/impermanence/rollback.nix
+++ profiles/impermanence/rollback.nix
@@ -1,31 +1,37 @@
-{
-  lib,
-  pkgs,
-  ...
-}: let
+{pkgs, ...}: let
   pool_name = "tank";
   root_snapshot_name = "${pool_name}/system/tmp@blank";
 in {
-  boot.initrd.systemd.enable = lib.mkDefault true;
-  boot.initrd.systemd.services.rollback = {
-    description = "Rollback root filesystem to a pristine state on boot";
-    wantedBy = [
-      # "zfs.target"
-      "initrd.target"
-    ];
-    after = [
-      "zfs-import-rpool.service"
-    ];
-    before = [
-      "sysroot.mount"
-    ];
-    path = [
-      pkgs.zfs
-    ];
-    unitConfig.DefaultDependencies = "no";
-    serviceConfig.Type = "oneshot";
-    script = ''
-      zfs rollback -r ${root_snapshot_name}
-    '';
+  boot.initrd = {
+    supportedFilesystems = ["zfs"]; # boot from zfs
+    # availableKernelModules = ["zfs" "spl"];
+    # kernelModules = ["zfs" "spl"];
+    # kernelModules = ["zfs"];
+    systemd = {
+      enable = true;
+      services = {
+        rollback = {
+          description = "Rollback filesystem to a pristine state on boot";
+          wantedBy = [
+            "zfs.target"
+            "initrd.target"
+          ];
+          after = [
+            "zfs-import-${pool_name}.service"
+          ];
+          before = [
+            "sysroot.mount"
+          ];
+          path = [
+            pkgs.zfs
+          ];
+          unitConfig.DefaultDependencies = "no";
+          serviceConfig.Type = "oneshot";
+          script = ''
+            zfs rollback -r ${root_snapshot_name}
+          '';
+        };
+      };
+    };
   };
 }
