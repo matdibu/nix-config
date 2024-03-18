945be180d3b797957f63940cfa553ab1a5439e83
Matei Dibu <contact@mateidibu.dev>
2024-03-18 18:05:27 +0200

    hosts: nix-iso: set ssh PermitRootLogin to "prohibit-password"

--- hosts/nix-iso/default.nix
+++ hosts/nix-iso/default.nix
@@ -1,11 +1,14 @@
 {
   inputs,
   pkgs,
+  lib,
   ...
 }: {
   imports = [
     "${inputs.nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
   ];
+
+  services.openssh.settings.PermitRootLogin = lib.mkForce "prohibit-password";
 
   environment.systemPackages = with pkgs; [
     gitMinimal
