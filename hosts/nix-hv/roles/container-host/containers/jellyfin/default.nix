92cb2b60919b250f105a0c02f497d742b2da2269
Matei Dibu <contact@mateidibu.dev>
2024-03-19 22:39:51 +0200

    hosts: nix-hv: jellyfin: add missing firewall ports

--- hosts/nix-hv/roles/container-host/containers/jellyfin/default.nix
+++ hosts/nix-hv/roles/container-host/containers/jellyfin/default.nix
@@ -3,7 +3,17 @@
     ./auto-generated.nix
   ];
 
-  networking.firewall.allowedTCPPorts = [8096];
+  # https://jellyfin.org/docs/general/networking/
+  networking.firewall = {
+    allowedTCPPorts = [
+    8096 # HTTP
+    8920 # HTTPS
+  ];
+    allowedUDPPorts = [
+    1900 # service auto-discovery
+    7359 # service auto-discovery
+  ];
+};
 
   systemd.targets."podman-jellyfin".unitConfig = {
     After = ["network-online.target" "systemd-networkd.service"];
