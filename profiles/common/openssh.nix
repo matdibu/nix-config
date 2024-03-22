5719e9f6a4cfe5e2a4d0201164ed4eebcfc8d6b7
Matei Dibu <contact@mateidibu.dev>
2024-03-22 10:46:38 +0200

    profiles: move ssh keys to unified file

--- profiles/common/openssh.nix
+++ profiles/common/openssh.nix
@@ -1,11 +1,7 @@
-let
-  yubi-main = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMW4GCCqGSeikOdpTizkcWcALL7QTaXRN9yLtXKip/bUAAAADXNzaDp5dWJpLW1haW4= ssh:yubi-main";
-  yubi-backup = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIExpu8Q76Btg0+3fVX0Upqvg/rUzmuijhzRoHk4g/KK3AAAAD3NzaDp5dWJpLWJhY2t1cA== ssh:yubi-backup";
-  pixel8 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILIuz4s1PzR1dXg/zVquHWXHnCaBJZbmWZrEAxoW3r5w pixel8";
-in {
+{
   users.users = {
     "root" = {
-      openssh.authorizedKeys.keys = [
+      openssh.authorizedKeys.keys = with (import ./ssh-keys.nix); [
         yubi-main
         yubi-backup
         pixel8
--- profiles/common/users.nix
+++ profiles/common/users.nix
@@ -2,11 +2,7 @@
   lib,
   config,
   ...
-}: let
-  yubi-main = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMW4GCCqGSeikOdpTizkcWcALL7QTaXRN9yLtXKip/bUAAAADXNzaDp5dWJpLW1haW4= ssh:yubi-main";
-  yubi-backup = "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIExpu8Q76Btg0+3fVX0Upqvg/rUzmuijhzRoHk4g/KK3AAAAD3NzaDp5dWJpLWJhY2t1cA== ssh:yubi-backup";
-  pixel8 = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILIuz4s1PzR1dXg/zVquHWXHnCaBJZbmWZrEAxoW3r5w pixel8";
-in {
+}: {
   users.users.mateidibu = {
     isNormalUser = true;
     uid = 1000;
@@ -14,7 +10,7 @@ in {
     extraGroups =
       ["wheel" "video"]
       ++ lib.optional config.virtualisation.libvirtd.enable "libvirtd";
-    openssh.authorizedKeys.keys = [
+    openssh.authorizedKeys.keys = with (import ./ssh-keys.nix); [
       yubi-main
       yubi-backup
       pixel8
