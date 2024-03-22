ea8c41ac041760d07d4d3e8f4232b421cb36b52e
Matei Dibu <contact@mateidibu.dev>
2024-03-22 10:56:25 +0200

    profiles: merge opengl into nvidia; cleanup

--- profiles/flake-module.nix
+++ profiles/flake-module.nix
@@ -3,7 +3,6 @@
     profiles-audio = ./audio.nix;
     profiles-common = ./common;
     profiles-nvidia = ./nvidia.nix;
-    profiles-opengl = ./opengl.nix;
     profiles-qemu-guest = ./qemu-guest.nix;
     profiles-zfs = ./zfs.nix;
   };
--- profiles/nvidia.nix
+++ profiles/nvidia.nix
@@ -3,36 +3,24 @@
   config,
   ...
 }: {
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    driSupport32Bit = true;
+  };
+
   nixpkgs.config.allowUnfree = true;
 
   services.xserver.videoDrivers = ["nvidia"];
 
-  boot.initrd.kernelModules = [
-    "nvidia"
-    "nvidia_modeset"
-    "nvidia_uvm"
-    "nvidia_drm"
-  ];
-
   boot.kernelParams = lib.mkBefore [
-    "nvidia_drm.modeset=1"
     "nvidia_drm.fbdev=1"
   ];
 
   hardware.nvidia = {
-    # Modesetting is required.
     modesetting.enable = true;
-
-    # Nvidia power management. Experimental, and can cause sleep/suspend to fail.
-    # Enable this if you have graphical corruption issues or application crashes after waking
-    # up from sleep. This fixes it by saving the entire VRAM memory to /tmp/ instead
-    # of just the bare essentials.
     powerManagement.enable = false;
-
-    # Fine-grained power management. Turns off GPU when not in use.
-    # Experimental and only works on modern Nvidia GPUs (Turing or newer).
     powerManagement.finegrained = false;
-
     open = true;
     nvidiaSettings = false;
     package = config.boot.kernelPackages.nvidiaPackages.beta;
