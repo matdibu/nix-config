34795714fff26f950de751811856f47402ffc856
Matei Dibu <contact@mateidibu.dev>
2024-03-24 20:59:48 +0200

    home: impermanence: temp. disable, was blocking builds

--- home/mateidibu/impermanence.nix
+++ home/mateidibu/impermanence.nix
@@ -2,10 +2,10 @@
   imports = [
     inputs.impermanence.nixosModules.home-manager.impermanence
   ];
-  home.persistence."${osConfig.impermanence.mountpoint}/home" = {
+  home.persistence."${osConfig.impermanence.mountpoint}" = {
     allowOther = true;
     files = [
-      ".ssh/id_ed25519_sk_rk_yubi-backup_mateidibu"
+      # ".ssh/id_ed25519_sk_rk_yubi-backup_mateidibu"
     ];
   };
 }
