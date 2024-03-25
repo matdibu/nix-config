ffd19cbe84228e887769329d4b4d1ecb2fe2839d
Matei Dibu <contact@mateidibu.dev>
2024-03-25 21:39:39 +0200

    hosts: nix-x570: add modules detected by lm_sensors

--- hosts/nix-x570/hardware/default.nix
+++ hosts/nix-x570/hardware/default.nix
@@ -19,7 +19,18 @@
     efi.canTouchEfiVariables = true;
   };
 
-  boot.initrd.kernelModules = [ "nvme" ];
+  boot.initrd.kernelModules = [
+    "nvme"
+    # Chip `AMD Family 17h thermal sensors' (confidence: 9)
+    "k10temp"
+    # ISA bus, address 0x290
+    # Chip `Nuvoton NCT6798D Super IO Sensors' (confidence: 9)
+    "nct6775"
+    # Bus `SMBus PIIX4 adapter port 1 at 0b20'
+    # Busdriver `i2c_piix4', I2C address 0x4f
+    # Chip `Dallas Semiconductor DS75' (confidence: 3)
+    "lm75"
+  ];
 
   hardware.enableAllFirmware = true;
 
