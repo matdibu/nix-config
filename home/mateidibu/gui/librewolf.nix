803996db3bcfd26df1a507558864bd7add13f239
Matei Dibu <contact@mateidibu.dev>
2024-03-27 12:47:17 +0200

    home: librewolf/sway: try to get hwaccel working

--- home/mateidibu/gui/librewolf.nix
+++ home/mateidibu/gui/librewolf.nix
@@ -3,34 +3,34 @@
     # Firefox wayland environment variable
     MOZ_ENABLE_WAYLAND = "1";
     MOZ_USE_XINPUT2 = "1";
-    # MOZ_DISABLE_RDD_SANDBOX = "1";
 
-    # NVD_BACKEND = "direct";
-    # LIBVA_DRIVER_NAME = "nvidia";
+    MOZ_DISABLE_RDD_SANDBOX = "1";
+    NVD_BACKEND = "direct";
+    LIBVA_DRIVER_NAME = "nvidia";
   };
   programs.librewolf = {
     enable = true;
-    #settings = {
-    #  "dom.webgpu.enabled" = true;
-    #  "dom.webgpu.workers.enabled" = true;
-    #  "webgl.disabled" = false;
-    #  "media.ffmpeg.vaapi.enabled" = true;
-    #  "media.rdd-ffmpeg.enabled" = true;
-    #  "media.av1.enabled" = true;
-    #  "media.hardware-video-decoding.force-enabled" = true;
-    #  "gfx.webgpu.ignore-blocklist" = true;
-    #  "gfx.webrender.all" = true;
-    #  "gfx.webrender.compositor" = true;
-    #  "gfx.webrender.compositor.force-enabled" = true;
-    #  "gfx.x11-egl.force-enabled" = true;
-    #  "layers.acceleration.force-enabled" = true;
-    #  "webgl.force-enabled" = true;
-    #  "widget.dmabuf.force-enabled" = true;
-    #  "widget.wayland.opaque-region.enabled" = false;
-    #  "privacy.resistFingerprinting" = false;
-    #  "privacy.clearOnShutdown.history" = false;
-    #  "privacy.clearOnShutdown.cookies" = false;
-    #  "network.cookie.lifetimePolicy" = 0;
-    #};
+    settings = {
+      "dom.webgpu.enabled" = true;
+      "dom.webgpu.workers.enabled" = true;
+      "webgl.disabled" = false;
+      "media.ffmpeg.vaapi.enabled" = true;
+      "media.rdd-ffmpeg.enabled" = true;
+      "media.av1.enabled" = true;
+      "media.hardware-video-decoding.force-enabled" = true;
+      "gfx.webgpu.ignore-blocklist" = true;
+      "gfx.webrender.all" = true;
+      "gfx.webrender.compositor" = true;
+      "gfx.webrender.compositor.force-enabled" = true;
+      "gfx.x11-egl.force-enabled" = true;
+      "layers.acceleration.force-enabled" = true;
+      "webgl.force-enabled" = true;
+      "widget.dmabuf.force-enabled" = true;
+      "widget.wayland.opaque-region.enabled" = false;
+      "privacy.resistFingerprinting" = false;
+      "privacy.clearOnShutdown.history" = false;
+      "privacy.clearOnShutdown.cookies" = false;
+      "network.cookie.lifetimePolicy" = 0;
+    };
   };
 }
--- home/mateidibu/gui/sway.nix
+++ home/mateidibu/gui/sway.nix
@@ -11,29 +11,28 @@
     };
   };
 
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    config = {
+      common.default = [ "wlr" "gtk" ];
+      sway.default = [ "wlr" "gtk" ];
+    };
+    configPackages = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+    ];
+    extraPortals = with pkgs; [
+      xdg-desktop-portal-wlr
+      xdg-desktop-portal-gtk
+    ];
+  };
+
   home.sessionVariables = {
-    # Set wlroots renderer to Vulkan to avoid flickering
-    WLR_RENDERER = "vulkan";
-    # Hardware cursors not yet working on wlroots
-    WLR_NO_HARDWARE_CURSORS = "1";
-
-    # Chromium/Electron
-    NIXOS_OZONE_WL = "1";
-
-    SDL_VIDEODRIVER = "wayland";
-
-    QT_QPA_PLATFORM = "wayland";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-
-    _JAVA_AWT_WM_NONREPARENTING = "1";
-
-    # Xwayland compatibility
-    XWAYLAND_NO_GLAMOR = "1";
-
-    # GBM_BACKEND = "nvidia-drm";
-    # __GL_GSYNC_ALLOWED = "0";
-    # __GL_VRR_ALLOWED = "0";
-    # __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    # GBM_BACKEND="nvidia-drm";
+    # __GL_GSYNC_ALLOWED="0";
+    # __GL_VRR_ALLOWED="0";
+    # __GLX_VENDOR_LIBRARY_NAME="nvidia";
   };
 
   home.packages = with pkgs; [
@@ -52,6 +51,8 @@
 
   wayland.windowManager.sway = {
     enable = true;
+    systemd.enable = true;
+    xwayland = false;
     config = {
       fonts = {
         names = [ "monospace" ];
@@ -73,7 +74,6 @@
 
       xwayland disable
 
-      # TV
       output 'LG Electronics LG TV SSCR2 0x01010101' disable
 
       output 'Dell Inc. DELL S2721DGF BS4J623' {
@@ -82,6 +82,25 @@
         # render_bit_depth 10
         subpixel rgb
       }
+    '';
+    extraSessionCommands = ''
+      # Set wlroots renderer to Vulkan to avoid flickering
+      export WLR_RENDERER="vulkan";
+      # Hardware cursors not yet working on wlroots
+      export WLR_NO_HARDWARE_CURSORS="1";
+
+      # Chromium/Electron
+      export NIXOS_OZONE_WL="1";
+
+      export SDL_VIDEODRIVER="wayland";
+
+      export QT_QPA_PLATFORM="wayland";
+      export QT_WAYLAND_DISABLE_WINDOWDECORATION="1";
+
+      export _JAVA_AWT_WM_NONREPARENTING="1";
+
+      # Xwayland compatibility
+      export XWAYLAND_NO_GLAMOR="1";
     '';
     wrapperFeatures.gtk = true;
   };
