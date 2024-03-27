507c96beaf6f9919fe8a183bc1d7f93a2f573a93
Matei Dibu <contact@mateidibu.dev>
2024-03-27 15:12:51 +0200

    home: mateidibu: configure impermanence conditionally

--- home/mateidibu/impermanence.nix
+++ home/mateidibu/impermanence.nix
@@ -1,11 +1,13 @@
-{ osConfig, inputs, ... }: {
+{ osConfig, inputs, lib, ... }: {
   imports = [
     inputs.impermanence.nixosModules.home-manager.impermanence
   ];
-  home.persistence."${osConfig.impermanence.mountpoint}" = {
-    allowOther = true;
-    files = [
-      # ".ssh/id_ed25519_sk_rk_yubi-backup_mateidibu"
-    ];
+  config = lib.mkIf osConfig.impermanence.enable {
+    home.persistence."${osConfig.impermanence.mountpoint}" = {
+      allowOther = true;
+      files = [
+        # ".ssh/id_ed25519_sk_rk_yubi-backup_mateidibu"
+      ];
+    };
   };
 }
