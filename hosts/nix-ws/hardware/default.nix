0b87b06562df50984ca758eb58d25e115af05c50
Matei Dibu <contact@mateidibu.dev>
2024-04-02 11:38:47 +0300

    hosts: nix-ws: impernanence.nix -> disk.nix

--- hosts/nix-ws/hardware/default.nix
+++ hosts/nix-ws/hardware/default.nix
@@ -1,6 +1,6 @@
 { inputs, ... }: {
   imports = (with inputs.self.nixosModules; [ profiles-qemu-guest ])
-    ++ [ ./networking.nix ./impermanence.nix ];
+    ++ [ ./networking.nix ./disk.nix ];
 
   modules.gpu-nvidia.enable = true;
 
@@ -8,11 +8,4 @@
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
-
-  modules.impermanence = {
-    enable = true;
-    device = "/dev/disk/by-path/virtio-pci-0000:00:07.0";
-  };
-
-  hardware.enableAllFirmware = true;
 }
--- hosts/nix-ws/hardware/disk.nix
+++ hosts/nix-ws/hardware/disk.nix
@@ -1,4 +1,9 @@
 { config, ... }: {
+  modules.impermanence = {
+    enable = true;
+    device = "/dev/disk/by-path/virtio-pci-0000:00:07.0";
+  };
+
   environment.persistence.${config.modules.impermanence.mountpoint} = {
     users."mateidibu" = { directories = [ "git" ]; };
   };
