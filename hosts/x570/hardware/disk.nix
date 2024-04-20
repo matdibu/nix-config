68a51fd182c8feda53995dd8077e3df7629e59dc
Matei Dibu <contact@mateidibu.dev>
2024-04-20 14:55:32 +0300

    hosts: x570: switch to btrfs-based impermanence

--- hosts/x570/hardware/disk.nix
+++ hosts/x570/hardware/disk.nix
@@ -3,7 +3,7 @@
   boot.initrd.kernelModules = [ "nvme" ];
 
   modules = {
-    impermanence = {
+    impermanence-btrfs = {
       enable = true;
       device = "/dev/disk/by-id/nvme-Samsung_SSD_970_EVO_Plus_1TB_S4EWNF0M943331J";
     };
