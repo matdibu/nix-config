e05e21f13e38409d6a7cfcd9b2558081ef96447d
Matei Dibu <contact@mateidibu.dev>
2024-04-20 17:21:09 +0300

    hosts: iso: fix config

--- hosts/iso/default.nix
+++ hosts/iso/default.nix
@@ -7,6 +7,12 @@
 {
   imports = [ "${modulesPath}/installer/cd-dvd/installation-cd-minimal.nix" ];
 
+  isoImage = {
+    makeBiosBootable = true;
+    makeEfiBootable = true;
+    makeUsbBootable = true;
+  };
+
   services.openssh.settings.PermitRootLogin = lib.mkForce "prohibit-password";
 
   boot.kernelPackages = config.boot.zfs.package.latestCompatibleLinuxPackages;
@@ -14,6 +20,7 @@
   # use DHCP on all interfaces
   networking.useDHCP = lib.mkForce true;
   systemd.network.enable = lib.mkForce false;
+  boot.initrd.systemd.enable = lib.mkForce false;
 
   system.stateVersion = "24.05";
 }
