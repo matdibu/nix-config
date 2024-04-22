cb0f115a69f7c484670ef553f5e781e40cdc118b
Matei Dibu <contact@mateidibu.dev>
2024-04-22 23:02:27 +0300

    hosts: vp4670: remove unnecessary kernel config

--- hosts/vp4670/default.nix
+++ hosts/vp4670/default.nix
@@ -1,12 +1,9 @@
-{ pkgs, ... }:
 {
   imports = [ ./hardware ];
 
   modules = {
     sway.enable = true;
   };
-
-  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   system.stateVersion = "24.05";
 }
