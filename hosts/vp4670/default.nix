738f97b72169c848e67713308a134963688fd464
Matei Dibu <contact@mateidibu.dev>
2024-04-22 21:40:12 +0300

    hosts: vp4670: use latest kernel

--- hosts/vp4670/default.nix
+++ hosts/vp4670/default.nix
@@ -1,9 +1,12 @@
+{ pkgs, ... }:
 {
   imports = [ ./hardware ];
 
   modules = {
     sway.enable = true;
   };
+
+  boot.kernelPackages = pkgs.linuxPackages_latest;
 
   system.stateVersion = "24.05";
 }
