2a8c2464545ec2449b166228fc5fb07cc51e10f5
Matei Dibu <contact@mateidibu.dev>
2024-04-22 21:52:15 +0300

    modules: impermanence-btrfs: fix rootfs partition id

--- nixosModules/impermanence-btrfs.nix
+++ nixosModules/impermanence-btrfs.nix
@@ -112,40 +112,46 @@ in
       after = [ "systemd-udevd.service" ];
       unitConfig.DefaultDependencies = "no";
       serviceConfig.Type = "oneshot";
-      script = ''
-        mkdir -p /mnt_btrfs
-        # We first mount the btrfs root to /mnt_btrfs
-        # so we can manipulate btrfs subvolumes.
-        mount -t btrfs -o subvol=/ ${config.disko.devices.disk."root-impermanence".device}-part1 /mnt_btrfs
-        # While we're tempted to just delete /rootfs and create
-        # a new snapshot from /root-blank, /rootfs is already
-        # populated at this point with a number of subvolumes,
-        # which makes `btrfs subvolume delete` fail.
-        # So, we remove them first.
-        #
-        # /rootfs contains subvolumes:
-        # - /rootfs/var/lib/portables
-        # - /rootfs/var/lib/machines
-        #
-        # I suspect these are related to systemd-nspawn, but
-        # since I don't use it I'm not 100% sure.
-        # Anyhow, deleting these subvolumes hasn't resulted
-        # in any issues so far, except for fairly
-        # benign-looking errors from systemd-tmpfiles.
-        btrfs subvolume list -o /mnt_btrfs/rootfs |
-          cut -f9 -d' ' |
-          while read subvolume; do
-            echo "deleting /$subvolume subvolume..."
-            btrfs subvolume delete "/mnt_btrfs/$subvolume"
-          done &&
-          echo "deleting /rootfs subvolume..." &&
-          btrfs subvolume delete /mnt_btrfs/rootfs
-        echo "creating blank /rootfs subvolume..."
-        btrfs subvolume create /mnt_btrfs/rootfs
-        # Once we're done rolling back to a blank snapshot,
-        # we can unmount /mnt_btrfs and continue on the boot process.
-        umount /mnt_btrfs
-      '';
+      script =
+        let
+          disk = config.disko.devices.disk."root-impermanence";
+        in
+        ''
+          mkdir -p /mnt_btrfs
+          # We first mount the btrfs root to /mnt_btrfs
+          # so we can manipulate btrfs subvolumes.
+          mount -t btrfs -o subvol=/ ${disk.device}-part${
+            toString disk.content.partitions."btrfs-root"._index
+          } /mnt_btrfs
+          # While we're tempted to just delete /rootfs and create
+          # a new snapshot from /root-blank, /rootfs is already
+          # populated at this point with a number of subvolumes,
+          # which makes `btrfs subvolume delete` fail.
+          # So, we remove them first.
+          #
+          # /rootfs contains subvolumes:
+          # - /rootfs/var/lib/portables
+          # - /rootfs/var/lib/machines
+          #
+          # I suspect these are related to systemd-nspawn, but
+          # since I don't use it I'm not 100% sure.
+          # Anyhow, deleting these subvolumes hasn't resulted
+          # in any issues so far, except for fairly
+          # benign-looking errors from systemd-tmpfiles.
+          btrfs subvolume list -o /mnt_btrfs/rootfs |
+            cut -f9 -d' ' |
+            while read subvolume; do
+              echo "deleting /$subvolume subvolume..."
+              btrfs subvolume delete "/mnt_btrfs/$subvolume"
+            done &&
+            echo "deleting /rootfs subvolume..." &&
+            btrfs subvolume delete /mnt_btrfs/rootfs
+          echo "creating blank /rootfs subvolume..."
+          btrfs subvolume create /mnt_btrfs/rootfs
+          # Once we're done rolling back to a blank snapshot,
+          # we can unmount /mnt_btrfs and continue on the boot process.
+          umount /mnt_btrfs
+        '';
     };
   };
 }
