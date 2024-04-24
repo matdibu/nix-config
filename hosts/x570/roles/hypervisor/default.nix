1a58c87d2a2939e0e8e99cd07948e95bedaa9d6b
Matei Dibu <contact@mateidibu.dev>
2024-04-24 09:11:06 +0300

    hosts: x570: tune kvm_amd options

--- hosts/x570/roles/hypervisor/default.nix
+++ hosts/x570/roles/hypervisor/default.nix
@@ -11,6 +11,15 @@
 
   environment.systemPackages = with pkgs; [ cloud-hypervisor ];
 
+  boot.kernelParams = [
+    "kvm_amd.avic=1"
+    "kvm_amd.npt=1"
+    "kvm_amd.nested=1"
+    # "kvm_amd.sev=1"
+    "kvm.ignore_msrs=1"
+    "kvm.report_ignored_msrs=1"
+  ];
+
   # boot.kernelParams = [
   #   "default_hugepagesz=1G"
   #   "hugepagesz=1G"
