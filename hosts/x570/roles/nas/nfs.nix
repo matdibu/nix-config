8f88fa1ffb5be23c5448fcf971aa17e77a85594c
Matei Dibu <contact@mateidibu.dev>
2024-04-24 14:35:36 +0300

    hosts: x570: nas: replace deprecated usage

--- hosts/x570/roles/nas/nfs.nix
+++ hosts/x570/roles/nas/nfs.nix
@@ -1,17 +1,21 @@
 {
-  services.nfs.server = {
-    enable = true;
-    exports = ''
-      /mnt/torrents 192.168.1.0/24(rw,insecure,no_subtree_check,pnfs,all_squash)
-    '';
-    extraNfsdConfig = ''
-      vers2=no
-      vers3=no
-      vers4=yes
-      vers4.0=yes
-      vers4.1=yes
-      vers4.2=yes
-    '';
+  services.nfs = {
+    server = {
+      enable = true;
+      exports = ''
+        /mnt/torrents 192.168.1.0/24(rw,insecure,no_subtree_check,pnfs,all_squash)
+      '';
+    };
+    settings = {
+      nfsd = {
+        "vers2" = false;
+        "vers3" = false;
+        "vers4" = true;
+        "vers4.0" = true;
+        "vers4.1" = true;
+        "vers4.2" = true;
+      };
+    };
   };
   networking.firewall.allowedTCPPorts = [ 2049 ];
 }
