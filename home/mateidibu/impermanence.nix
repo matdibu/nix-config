ca805d7633600e93b7c1ca86927ca18f5aad90f3
Matei Dibu <contact@mateidibu.dev>
2024-04-26 16:36:33 +0300

    home: mateidibu: impermanence: fix base path

--- home/mateidibu/impermanence.nix
+++ home/mateidibu/impermanence.nix
@@ -7,7 +7,7 @@
 {
   imports = [ inputs.impermanence.nixosModules.home-manager.impermanence ];
   config = lib.mkIf osConfig.modules.impermanence.enable {
-    home.persistence."${osConfig.modules.impermanence.mountpoint}" = {
+    home.persistence."${osConfig.modules.impermanence.mountpoint}/home/mateidibu" = {
       allowOther = true;
       files = [ ".ssh/id_ed25519_sk_rk_yubi-backup_mateidibu" ];
       directories = [
