c779f2ffd6423041e50278448f814606be3b3252
Matei Dibu <contact@mateidibu.dev>
2024-04-27 14:03:56 +0300

    hosts: x570: use nixos-hardware module

--- hosts/x570/hardware/default.nix
+++ hosts/x570/hardware/default.nix
@@ -1,22 +1,15 @@
 { inputs, ... }:
 {
-  imports =
-    (with inputs.nixos-hardware.nixosModules; [
-      common-pc
-      common-pc-ssd
-      common-cpu-amd-pstate
-    ])
-    ++ [
-      ./disk-rootfs.nix
-      ./disk-nas.nix
-      ./networking.nix
-    ];
+  imports = [
+    inputs.nixos-hardware.nixosModules.asus-pro-ws-x570-ace
+    ./disk-rootfs.nix
+    ./disk-nas.nix
+    ./networking.nix
+  ];
 
   modules = {
     gpu-nvidia.enable = true;
   };
-
-  hardware.cpu.amd.updateMicrocode = true;
 
   boot.loader = {
     systemd-boot.enable = true;
