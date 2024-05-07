ba70491be7e7866839a05273874a08c4e9b426cd
Matei Dibu <contact@mateidibu.dev>
2024-05-07 18:16:34 +0300

    hosts: x570: add specialisations

--- hosts/x570/default.nix
+++ hosts/x570/default.nix
@@ -1,15 +1,22 @@
-{ pkgs, ... }:
+{ self, pkgs, ... }:
 {
   imports = [
     ./hardware
-    # ./roles/hypervisor
     ./roles/nas
     ./roles/container-host
   ];
 
-  programs.steam = {
-    enable = true;
-    extraCompatPackages = with pkgs; [ proton-ge-bin ];
+  specialisation = {
+    "vfio".configuration = {
+      imports = [ ./roles/hypervisor ];
+    };
+    "steam".configuration = {
+      imports = [ self.nixosModules.profiles-gui ];
+      programs.steam = {
+        enable = true;
+        extraCompatPackages = with pkgs; [ proton-ge-bin ];
+      };
+    };
   };
 
   system.stateVersion = "24.05";
