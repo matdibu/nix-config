83a244c22493dd62114dcc375dc3c079b146bd4d
Matei Dibu <contact@mateidibu.dev>
2024-05-11 17:34:50 +0300

    dev: fix deploy script

--- dev/flake-module.nix
+++ dev/flake-module.nix
@@ -29,34 +29,49 @@
       # build shell scripts for deployment on each host, named "deploy-$host"
       apps =
         let
-          buildHost = self.nixosConfigurations.x570.configuration.networking.hostName;
-          script = host: cfg: ''
-            set -x
-
-            # default to 'switch'
-            if [[ -n "$1" ]]; then
-              TASK="$1"
-            else
-              TASK="switch"
-            fi
-
-            # force pseudo-terminal allocation (man 1 ssh)
-            ${lib.optionalString cfg.config.security.sudo.wheelNeedsPassword "export NIX_SSHOPTS=-tt"}
-
-            # run nixos-rebuild
-            ${lib.getExe (pkgs.nixos-rebuild.override { nix = pkgs.nixUnstable; })} \
-              "$TASK" \
-              --fast \
-              --flake ${inputs.self}#${host} \
-              --use-remote-sudo \
-              --target-host ${host}.lan \
-              --build-host "${buildHost}.lan"
-              # temporarily use ${buildHost} for all builds, since it's the most powerful
-          '';
+          hosts = lib.attrsets.filterAttrs(
+            name: value: (!lib.strings.hasInfix "iso" name && !lib.strings.hasInfix "sd-card" name && value.config.nixpkgs.buildPlatform.system == "x86_64-linux")
+          ) inputs.self.nixosConfigurations;
         in
-        lib.mapAttrs' (host: cfg: {
-          name = "deploy-${host}";
-          value.program = toString (pkgs.writeShellScript "deploy-${host}" (script host cfg));
-        }) inputs.self.nixosConfigurations;
+        (
+          let
+            user = "mateidibu";
+            script = host: cfg: ''
+              set -x
+
+              # default to 'boot'
+              if [[ -n "$1" ]]; then
+                TASK="$1"
+              else
+                TASK="boot"
+              fi
+
+              # force pseudo-terminal allocation (man 1 ssh)
+              # export NIX_SSHOPTS="-tt"
+
+              # run nixos-rebuild
+              ${lib.getExe pkgs.nixos-rebuild} \
+                "$TASK" \
+                --fast \
+                --flake ${inputs.self}#${host} \
+                --use-remote-sudo \
+                --target-host "${user}@${cfg.config.networking.hostName}.lan"
+            '';
+          in
+          lib.mapAttrs' (host: cfg: {
+            name = "deploy-${host}";
+            value.program = toString (pkgs.writeShellScript "deploy-${host}" (script host cfg));
+          }) hosts
+        )
+        // (
+          let
+            hostNames = lib.mapAttrsToList (name: _value: name) hosts;
+            script =
+              "set -x\n" + lib.strings.concatLines (map (host: inputs.self.apps."x86_64-linux"."deploy-${host}".program) hostNames);
+          in
+          {
+            "deploy-all".program = toString (pkgs.writeShellScript "deploy-all" script);
+          }
+        );
     };
 }
