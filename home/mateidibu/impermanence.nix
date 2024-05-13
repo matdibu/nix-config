c9817645c733e2dab3ae609398372f47cda10728
Matei Dibu <contact@mateidibu.dev>
2024-05-13 19:35:55 +0300

    wip

--- home/mateidibu/impermanence.nix
+++ home/mateidibu/impermanence.nix
@@ -11,7 +11,7 @@
       allowOther = true;
       files = [
         ".ssh/id_ed25519_sk_rk_yubi-backup_mateidibu"
-        ".ssh/id_ed25519_sk_rk_yubi-fast-backup_mateidibu"
+        ".ssh/id_ed25519_sk_rk_yubi-backup-fast_mateidibu"
       ];
       directories = [
         #(lib.optional osConfig.programs.steam.enable ".local/share/Steam")
