1ab14beb6de2194f99b74f8c51e5cf11bb97fda9
Matei Dibu <contact@mateidibu.dev>
2024-05-14 12:48:34 +0300

    wip

--- hosts/x570/overlays/qemu-iommufd.nix
+++ hosts/x570/overlays/qemu-iommufd.nix
@@ -2,7 +2,7 @@
   nixpkgs.overlays = [
     (_: prev: {
       qemu_kvm = prev.qemu_kvm.overrideAttrs (_: {
-        configureFlags = prev.qemu_kvm.configureFlags ++ [ "--enable-iommufd" ];
+        configureFlags = prev.qemu_kvm.configureFlags ++ [ "-DCONFIG_IOMMUFD" ];
       });
     })
   ];
