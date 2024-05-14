c30f01f6199514bcb2809e994cb952a2b3878449
Matei Dibu <contact@mateidibu.dev>
2024-05-14 12:57:48 +0300

    wip

--- hosts/x570/overlays/default.nix
+++ hosts/x570/overlays/default.nix
@@ -1,6 +1,5 @@
 {
   imports = [
     ./openmw.nix
-    ./qemu-iommufd.nix
   ];
 }
--- hosts/x570/overlays/qemu-iommufd.nix
+++
@@ -1,9 +0,0 @@
-{
-  nixpkgs.overlays = [
-    (_: prev: {
-      qemu_kvm = prev.qemu_kvm.overrideAttrs (_: {
-        configureFlags = prev.qemu_kvm.configureFlags ++ [ "-DCONFIG_IOMMUFD" ];
-      });
-    })
-  ];
-}
