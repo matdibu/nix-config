d615c820defe78ffe0972b869e7244a3bec5ed5f
Matei Dibu <contact@mateidibu.dev>
2024-05-19 16:27:20 +0300

    hosts: x570: remove amd_pstate

--- hosts/x570/hardware/default.nix
+++ hosts/x570/hardware/default.nix
@@ -5,17 +5,12 @@
     ./disk-rootfs.nix
     ./disk-nas.nix
     ./networking.nix
-    # ./amd_pstate.nix # didn't improve performance
   ];
 
   boot.loader = {
     systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
   };
-
-  boot.kernelParams = [ "amd_pstate=guided" ];
-
-  powerManagement.cpuFreqGovernor = "performance";
 
   hardware.rasdaemon.enable = true;
 }
