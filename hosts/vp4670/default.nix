a8d2cfd53cb94a98abbe65d0801c6d4d1388cf91
Matei Dibu <contact@mateidibu.dev>
2024-06-23 18:09:17 +0300

    hosts: vp4670: add nas

--- hosts/vp4670/default.nix
+++ hosts/vp4670/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./hardware
     ./containers
+    ./nas
   ];
 
   system.stateVersion = "24.05";
---
+++ hosts/vp4670/nas/samba.nix
@@ -0,0 +1,24 @@
+{
+  networking.firewall.allowPing = true;
+
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.samba = {
+    enable = true;
+    openFirewall = true;
+    extraConfig = ''
+      min protocol = smb3_11
+      guest ok = yes
+      guest only = yes
+    '';
+    shares = {
+      "torrents" = {
+        "path" = "/mnt/persist/torrents";
+        "writeable" = "no";
+      };
+    };
+  };
+}
