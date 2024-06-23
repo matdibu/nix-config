f121cb97c68b20939d764a90527ba24c16226e72
Matei Dibu <contact@mateidibu.dev>
2024-06-23 12:16:58 +0300

    hosts: x570: improve nfs share

--- hosts/x570/roles/nas/nfs.nix
+++ hosts/x570/roles/nas/nfs.nix
@@ -3,7 +3,8 @@
     server = {
       enable = true;
       exports = ''
-        /mnt/torrents 192.168.1.0/24(rw,insecure,no_subtree_check,pnfs,all_squash)
+        /mnt          192.168.1.0/24(rw,fsid=0,no_subtree_check)
+        /mnt/torrents 192.168.1.0/24(rw,nohide,insecure,no_subtree_check,pnfs,all_squash)
       '';
     };
     settings = {
@@ -11,8 +12,8 @@
         "vers2" = false;
         "vers3" = false;
         "vers4" = true;
-        "vers4.0" = true;
-        "vers4.1" = true;
+        "vers4.0" = false;
+        "vers4.1" = false;
         "vers4.2" = true;
       };
     };
