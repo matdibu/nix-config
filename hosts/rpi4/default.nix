08532748892bf864c45cbf27128958186cda8fff
Matei Dibu <contact@mateidibu.dev>
2024-06-24 20:54:21 +0300

    hosts: rpi4: wip

--- hosts/rpi4/default.nix
+++ hosts/rpi4/default.nix
@@ -1,5 +1,17 @@
+{ modulesPath, ... }:
 {
-  imports = [ ./hardware.nix ];
+  imports = [
+    ./hardware.nix
+    "${modulesPath}/image/repart.nix"
+  ];
+
+  modules = {
+    impermanence = {
+      enable = true;
+      type = "btrfs";
+      device = "/dev/disk/by-id/usb-Samsung_Flash_Drive_FIT_0342121020007839-0:0";
+    };
+  };
 
   system.stateVersion = "24.11";
 }
--- hosts/rpi4/hardware.nix
+++ hosts/rpi4/hardware.nix
@@ -1,35 +1,30 @@
-{ inputs, ... }:
+{ inputs, pkgs, ... }:
 {
   imports = [ inputs.nixos-hardware.nixosModules.raspberry-pi-4 ];
 
   hardware.raspberry-pi."4" = {
     apply-overlays-dtmerge.enable = true;
-    fkms-3d.enable = true;
+    # fkms-3d.enable = true;
     xhci.enable = true;
   };
 
   hardware.deviceTree = {
     enable = true;
-    filter = "*rpi-4-*.dtb";
+    # filter = "*rpi-4-*.dtb";
   };
 
   boot = {
+    # kernelPackages = pkgs.linuxPackages_rpi4;
+    kernelPackages = pkgs.linuxPackages_latest;
     initrd.availableKernelModules = [ "xhci_pci" ];
     loader = {
       systemd-boot.enable = true;
+      generic-extlinux-compatible.enable = false;
       efi.canTouchEfiVariables = true;
     };
   };
 
-  modules = {
-    impermanence = {
-      enable = true;
-      type = "btrfs";
-      device = "/dev/null";
-    };
-  };
-
-  networking.hostId = "9edbfeb8";
+  networking.hostId = "9edbfeb9";
 
   systemd.network.networks."10-wan" = {
     matchConfig.Name = "end0";
