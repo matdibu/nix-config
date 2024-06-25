5b1be50cf841c2b7234ee1d240148df254ea3658
Matei Dibu <contact@mateidibu.dev>
2024-06-25 15:14:28 +0300

    hosts: rpi4: wip

--- hosts/rpi4/klipper.nix
+++ hosts/rpi4/klipper.nix
@@ -1,4 +1,4 @@
-{lib, ...}:
+{ lib, ... }:
 {
   networking.firewall.allowedTCPPorts = [ 80 ];
 
@@ -18,6 +18,25 @@
     moonraker = {
       enable = true;
       user = "root";
+      settings = {
+        authorization = {
+          cors_domains = [
+            "*.local"
+            "*.lan"
+            "*://app.fluidd.xyz"
+            "*://my.mainsail.xyz"
+          ];
+          trusted_clients = [
+            "10.0.0.0/8"
+            "127.0.0.0/8"
+            "169.254.0.0/16"
+            "172.16.0.0/12"
+            "192.168.1.0/24"
+            "FE80::/10"
+            "::1/128"
+          ];
+        };
+      };
     };
     mainsail.enable = true;
     fluidd.enable = true;
