138a23851793f217d4e6fabdbe302b375aa2ffe5
Matei Dibu <contact@mateidibu.dev>
2024-06-25 13:11:43 +0300

    hosts: rpi4: add config

--- hosts/rpi4/klipper.nix
+++ hosts/rpi4/klipper.nix
@@ -2,7 +2,13 @@
   services = {
     klipper = {
       enable = true;
-      settings = { };
+      configFile = ./creality-v4.2.7.cfg;
+      firmwares = {
+        "ender3" = {
+            enable = true;
+            configFile = ./creality-v4.2.7.cfg;
+        };
+      };
     };
     moonraker.enable = true;
     mainsail.enable = true;
