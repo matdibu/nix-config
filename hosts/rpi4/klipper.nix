33d1b00862df318066d740247e00c8740032d52c
Matei Dibu <contact@mateidibu.dev>
2024-06-29 21:58:44 +0300

    hosts: rpi4: move klipper overlay into klipper.nix

--- hosts/rpi4/klipper.nix
+++ hosts/rpi4/klipper.nix
@@ -1,8 +1,15 @@
-{ lib, ... }:
+{ lib, pkgs, ... }:
 {
   networking.firewall.allowedTCPPorts = [ 80 ];
 
   networking.firewall.enable = lib.mkForce false;
+
+  nixpkgs.overlays = [
+    (_final: prev: {
+      klipper-firmware = prev.klipper-firmware.override { gcc-arm-embedded = pkgs.gcc-arm-embedded-11; };
+    })
+  ];
+
 
   services = {
     klipper = {
