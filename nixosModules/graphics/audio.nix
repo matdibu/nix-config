5e296400ba8fa43d7c4f96c2131336c4a808dbc2
Matei Dibu <contact@mateidibu.dev>
2024-07-15 14:21:41 +0300

    modules: audio: fix

--- nixosModules/graphics/audio.nix
+++ nixosModules/graphics/audio.nix
@@ -1,20 +1,17 @@
 { config, lib, ... }:
 {
   options = {
-    modules.audio.enable = lib.mkEnableOption "pipewire, emulating pulseaudio and alsa";
+    modules.audio.enable = lib.mkEnableOption "pipewire";
   };
   config = lib.mkIf config.modules.audio.enable {
     services.pipewire = {
       enable = true;
       alsa.enable = true;
-      # alsa.support32Bit = true;
       pulse.enable = true;
+      jack.enable = true;
     };
 
     # disable pulseaudio
     hardware.pulseaudio.enable = false;
-
-    # disable ALSA
-    sound.enable = false;
   };
 }
