502897f1b2d97188da27bc8207dd29ae0ba740a9
Matei Dibu <contact@mateidibu.dev>
2024-07-17 23:31:34 +0300

    hosts: add ISO's for real hosts

--- hosts/flake-module.nix
+++ hosts/flake-module.nix
@@ -35,11 +35,6 @@ in
       system = "aarch64-linux";
       hostName = "iso";
     };
-    iso-vp4670 = mkNixosSystem {
-      system = "x86_64-linux";
-      hostName = "iso";
-      offlineTarget = "vp4670";
-    };
     # sd-card-aarch64 = mkNixosSystem {
     #   system = "aarch64-linux";
     #   hostName = "sd-card";
@@ -48,17 +43,37 @@ in
       system = "x86_64-linux";
       hostName = "x570";
     };
+    iso-x570 = mkNixosSystem {
+      system = "x86_64-linux";
+      hostName = "iso";
+      offlineTarget = "x570";
+    };
     vp4670 = mkNixosSystem {
       system = "x86_64-linux";
       hostName = "vp4670";
+    };
+    iso-vp4670 = mkNixosSystem {
+      system = "x86_64-linux";
+      hostName = "iso";
+      offlineTarget = "vp4670";
     };
     rockpro64 = mkNixosSystem {
       system = "aarch64-linux";
       hostName = "rockpro64";
     };
+    iso-rockpro64= mkNixosSystem {
+      system = "aarch64-linux";
+      hostName = "iso";
+      offlineTarget = "rockpro64";
+    };
     rpi4 = mkNixosSystem {
       system = "aarch64-linux";
       hostName = "rpi4";
+    };
+    iso-rpi4 = mkNixosSystem {
+      system = "aarch64-linux";
+      hostName = "rpi4";
+      offlineTarget = "rpi4";
     };
   };
 }
