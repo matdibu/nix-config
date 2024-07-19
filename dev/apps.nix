20823995fac902033e326149eed81a7dd488fd61
Matei Dibu <contact@mateidibu.dev>
2024-07-20 00:16:57 +0300

    dev: create more helper apps

--- dev/apps.nix
+++ dev/apps.nix
@@ -21,8 +21,12 @@
           real-hosts = filter (name: (!hasPrefix "iso-" name && !hasPrefix "sd-card-" name)) (
             attrNames inputs.self.nixosConfigurations
           );
+          iso-hosts = filter (name: (hasPrefix "iso-" name)) (
+            attrNames inputs.self.nixosConfigurations
+          );
           user = "mateidibu";
         in
+                    lib.mkMerge [
         # apps "nixos-rebuild-${system}"
         (
           let
@@ -56,7 +60,7 @@
           )
         )
         # apps "reboot-${system}"
-        // (
+        (
           let
             reboot-script = host: ''
               set -ex
@@ -80,29 +84,58 @@
             }) real-hosts
           )
         )
-        # app "nixos-rebuild-all"
-        // (
+        # app "nixos-build-isos"
+        (
           let
-            host-to-prebuild-command =
+            host-to-iso-build-command =
               host:
-              pkgs.writeShellScript "nix-build-${host}" ''
+              pkgs.writeShellScript "nixos-build-${host}" ''
+                nix build \
+                    --out-link "${host}" \
+                    .#nixosConfigurations."${host}".config.system.build.toplevel
+              '';
+          in
+          {
+            "nixos-build-isos".program = pkgs.writeShellScriptBin "nixos-build-isos" ''
+              set -x
+              ${concatLines (map host-to-iso-build-command iso-hosts)}
+            '';
+          }
+        )
+        # app "nixos-build-all"
+        (
+          let
+            host-to-build-command =
+              host:
+              pkgs.writeShellScript "nixos-build-${host}" ''
                 nix build \
                     --no-link \
                     .#nixosConfigurations."${host}".config.system.build.toplevel
               '';
+          in
+          {
+            "nixos-build-all".program = pkgs.writeShellScriptBin "nixos-rebuild-all" ''
+              set -x
+              ${concatLines (map host-to-build-command real-hosts)}
+            '';
+          }
+        )
+        # app "nixos-rebuild-all"
+        (
+          let
             host-to-rebuild-app = host: (inputs.self.apps.${system}."nixos-rebuild-${host}".program + " $@");
           in
           {
             "nixos-rebuild-all".program = pkgs.writeShellScriptBin "nixos-rebuild-all" ''
               set -x
               # do the builds before deploying, to batch the 2FA requests at the end
-              ${concatLines (map host-to-prebuild-command real-hosts)}
+              nixos-build-all $@
               ${concatLines (map host-to-rebuild-app real-hosts)}
             '';
           }
         )
         # app "reboot-all"
-        // (
+        (
           let
             host-to-reboot-app = host: (inputs.self.apps.${system}."reboot-${host}".program + " $@");
           in
@@ -112,6 +145,6 @@
               ${concatLines (map host-to-reboot-app real-hosts)}
             '';
           }
-        );
+        )];
     };
 }
