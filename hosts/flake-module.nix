1be9cd40c19738a7c47c1f37b7647cf6f2a5e7ca
Matei Dibu <contact@mateidibu.dev>
2024-07-31 11:44:14 +0300

    hosts: iso: always enable offlineInstallers

--- hosts/flake-module.nix
+++ hosts/flake-module.nix
@@ -1,12 +1,10 @@
 { inputs, ... }:
 let
-  # wrapper over 'nixosSystem', with default configs and imports
   mkNixosSystem =
     args:
     inputs.nixpkgs.lib.nixosSystem {
       specialArgs = {
         inherit inputs;
-        offlineInstallers = args.offlineInstallers or null;
       };
       inherit (args) system;
       modules = [
@@ -30,12 +28,10 @@ in
     iso-offlineInstallers-x86_64 = mkNixosSystem {
       system = "x86_64-linux";
       hostName = "iso";
-      offlineInstallers = true;
     };
     iso-offlineInstallers-aarch64 = mkNixosSystem {
       system = "aarch64-linux";
       hostName = "iso";
-      offlineInstallers = true;
     };
     x570 = mkNixosSystem {
       system = "x86_64-linux";
--- hosts/iso/offlineInstallers.nix
+++ hosts/iso/offlineInstallers.nix
@@ -3,7 +3,6 @@
   pkgs,
   lib,
   config,
-  offlineInstallers,
   ...
 }:
 let
@@ -20,37 +19,34 @@ let
   ) (lib.attrNames inputs.self.nixosConfigurations);
 in
 {
-  config = lib.mkIf offlineInstallers {
+  environment.etc."install-closure".source =
+    let
+      targetDerivations = target: [
+        inputs.self.nixosConfigurations.${target}.config.system.build.toplevel
+        inputs.self.nixosConfigurations.${target}.config.system.build.diskoScript
+        inputs.self.nixosConfigurations.${target}.config.system.build.diskoScript.drvPath
+        inputs.self.nixosConfigurations.${target}.pkgs.stdenv.drvPath
+        (inputs.self.nixosConfigurations.${target}.pkgs.closureInfo { rootPaths = [ ]; }).drvPath
+      ];
 
-    environment.etc."install-closure".source =
-      let
-        targetDerivations = target: [
-          inputs.self.nixosConfigurations.${target}.config.system.build.toplevel
-          inputs.self.nixosConfigurations.${target}.config.system.build.diskoScript
-          inputs.self.nixosConfigurations.${target}.config.system.build.diskoScript.drvPath
-          inputs.self.nixosConfigurations.${target}.pkgs.stdenv.drvPath
-          (inputs.self.nixosConfigurations.${target}.pkgs.closureInfo { rootPaths = [ ]; }).drvPath
-        ];
+      flakeInputsDerivations = builtins.map (i: i.outPath) (builtins.attrValues inputs.self.inputs);
 
-        flakeInputsDerivations = builtins.map (i: i.outPath) (builtins.attrValues inputs.self.inputs);
+      dependencies = (lib.concatMap targetDerivations targets) ++ flakeInputsDerivations;
 
-        dependencies = (lib.concatMap targetDerivations targets) ++ flakeInputsDerivations;
+      closureInfo = pkgs.closureInfo { rootPaths = dependencies; };
+    in
+    "${closureInfo}/store-paths";
 
-        closureInfo = pkgs.closureInfo { rootPaths = dependencies; };
-      in
-      "${closureInfo}/store-paths";
-
-    environment.systemPackages =
-      let
-        scriptBin =
-          target:
-          (pkgs.writeShellScriptBin "install-nixos-unattended-${target}" ''
-            set -eux
-            exec ${pkgs.disko}/bin/disko-install --flake "${inputs.self}#${target}" --disk "root-disk" ${
-              inputs.self.nixosConfigurations.${target}.config.disko.devices.disk."root-disk".device
-            }
-          '');
-      in
-      map scriptBin targets;
-  };
+  environment.systemPackages =
+    let
+      scriptBin =
+        target:
+        (pkgs.writeShellScriptBin "install-nixos-unattended-${target}" ''
+          set -eux
+          exec ${pkgs.disko}/bin/disko-install --flake "${inputs.self}#${target}" --disk "root-disk" ${
+            inputs.self.nixosConfigurations.${target}.config.disko.devices.disk."root-disk".device
+          }
+        '');
+    in
+    map scriptBin targets;
 }
